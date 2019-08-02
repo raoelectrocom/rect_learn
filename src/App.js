0df6c086ca63e527e654b579c06ae55bd83b608f
Kondala Rao <raoelectrocom@gmail.com>
2019-08-02 07:01:18 +0530

    creating functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         I am react app
       </h1>
       <p>This is really working</p>
+      <Person />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', 'null', 'Does this work now?'));
